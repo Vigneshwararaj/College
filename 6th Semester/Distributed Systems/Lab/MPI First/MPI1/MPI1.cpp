5435f71fc6a779c280a09cf44923893a0ceea8ca
surya3001 <suryab2001@gmail.com>
2022-01-04 12:47:51 +0530

    commit

--- 6th Semester/Distributed Systems/Lab/MPI First/MPI1/MPI1.cpp
+++ 6th Semester/Distributed Systems/Lab/MPI First/MPI1/MPI1.cpp
@@ -1,21 +1,30 @@
 // MPI1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-#include "mpi.h"
-#include<stdio.h>
+#include <mpi.h>
+#include <stdio.h>
 
-int main()
-{
-    int commsize;
-    int rank;
-
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
     MPI_Init(NULL, NULL);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &commsize);
 
-    //printf("Hello World");
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    std::cout << "Hello World!\n";
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print off a hello world message
+    printf("Hello world from processor %s, rank %d out of %d processors\n",
+        processor_name, world_rank, world_size);
+
+    // Finalize the MPI environment.
     MPI_Finalize();
 }
